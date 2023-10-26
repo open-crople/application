7aed6bd1a3c048e8e1ad05d83a56f586dbfac822
박보영 <pby0204@gmail.com>
2023-10-26 12:33:04 +0000

    그룹과제1

---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import sys
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return render_template("buy_items_list.html")
+
+
+@application.route("/rent_items_list")
+def rent_list():
+    return render_template("rent_items_list.html")
+
+
+@application.route("/mypage")
+def view_mypage():
+    return render_template("mypage.html")
+
+@application.route("/reg_reviews")
+def reg_reviews():
+    return render_template("reg_reviews.html")
+
+
+@application.route("/submit_item_post", methods=['POST'])
+def reg_item_submit_post():
+    image_file = request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    data = request.form
+    print(request.form["name"], request.form["price"], request.form["addr"], request.form["keyword"], request.form["explain"])
+    return render_template("result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', debug=True)
